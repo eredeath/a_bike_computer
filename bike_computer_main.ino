dc37eb1eec116ede8910927751b248f3bd3a5c8d
Ryan Jonesford <eredeath@users.noreply.github.com>
2016-01-09 11:57:11 -0500

    updated with lcd screen, Adding schematic

--- bike_computer_main.ino
+++ bike_computer_main.ino
@@ -1,10 +1,15 @@
 //Bike computer project
+// include the library code:
+#include <LiquidCrystal.h>
 
-#define WHEEL_CIR_IN_MM 2155.0
+// initialize the library with the numbers of the interface pins
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+#define WHEEL_CIR_IN_MM 2194.4
 #define NUM_OF_TIME_SAMPLES 4
 
 short led = 13;
-short mag_switch = 2;
+short mag_switch = 7;
 short switch_made;
 short inc_count = true;
 unsigned long mm = 0;
@@ -19,13 +24,29 @@ unsigned long this_time = 0;
 unsigned int avg_time = 0;
 unsigned int numerator = 0;
 
-short imperial = false;
-//short imperial = true;
-//short sim_pulse = false;
-short sim_pulse = true;
-int sim_delay = 500;
+//short imperial = false;
+short imperial = true;
+short sim_pulse = false;
+//short sim_pulse = true;
+int sim_delay = 50;
 
-void setup(){
+void setup()
+{
+  // set up the LCD's number of columns and rows: 
+  lcd.begin(16, 2);
+  if(!imperial)
+  {
+    lcd.setCursor(2, 0);
+    lcd.print("km");
+    lcd.setCursor(8,0);
+    lcd.print("m");
+  }
+  else
+  {
+    lcd.setCursor(4,1);
+    lcd.print("mi");
+  }
+  
   pinMode(led, OUTPUT);
   pinMode(mag_switch, INPUT);
   Serial.begin(9600);
@@ -33,6 +54,7 @@ void setup(){
 
 void loop(){
   this_time = millis();
+      
   if((this_time - last_time) > 10000)
   {
     dd_dt = 0.0;
@@ -43,7 +65,14 @@ void loop(){
   }
   numerator = 0;
   switch_made = digitalRead(mag_switch);
-  digitalWrite(led, switch_made);
+  if(sim_pulse)
+  {
+    digitalWrite(led, sim_pulse);
+  }
+  else
+  {
+    digitalWrite(led, switch_made);
+  }
   if(switch_made || sim_pulse)
   {
     if(inc_count)
@@ -86,19 +115,21 @@ void loop(){
   }
   if(sim_pulse)
   {
-    delay(sim_delay);
-    if(millis() >= 60000)
-    {  
-      inc_count = false;
-      sim_pulse = false;
-    }
-    else
-      inc_count = true;
+    delay(sim_delay/2);
+    digitalWrite(led,0);
+    delay(sim_delay/2);
+    inc_count = true;
   }
   if(imperial)
   {
     miles = (.0006*meters + (kmeters*1000));
     mph = .6214 * dd_dt;
+    lcd.setCursor(0,0);
+    lcd.print("MPH = ");
+    lcd.print(mph);
+    lcd.setCursor(0,1);
+    lcd.print(miles);
+    lcd.print("mi");
     Serial.print("Miles: = ");
     Serial.println(miles);
     Serial.print("MPH: = ");
@@ -106,12 +137,20 @@ void loop(){
   }
   else
   {
+    lcd.setCursor(0, 0);
+    lcd.print(kmeters);
     Serial.print("kmeters = ");
     Serial.print(kmeters);
+    lcd.setCursor(5, 0);
+    lcd.print(meters);
     Serial.print(", Meters = ");
     Serial.print(meters);
     Serial.print(", mm = ");
     Serial.println(mm);
+    lcd.setCursor(0, 1);
+    lcd.print(dd_dt);
+    lcd.setCursor(5, 1);
+    lcd.print("km/hr");
     Serial.print("km/hr = ");
     Serial.println(dd_dt);
   }
